10c0fd49c176f6135d0614d0a384783a4bff7dfc
John Dunnicliffe <dunnicli@gmail.com>
2021-11-15 19:35:44 -0600

    Div Links - They work now.

--- pages/manager/nfts/tokens/index.js
+++ pages/manager/nfts/tokens/index.js
@@ -52,18 +52,22 @@ export default function Page({ data }) {
               key="item.id"
               className="max-w-sm w-full lg:max-w-full lg:flex my-5"
             >
-              <div
-                className="h-48 lg:h-auto lg:w-48 flex-none bg-cover 
+              <Link href={`/manager/nfts/tokens/detailsToken/${item.id}`}>
+                <div
+                  className="h-48 lg:h-auto lg:w-48 flex-none bg-cover 
                 rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-                style={{
-                  ...(item.metaImageUrl
-                    ? {
-                        backgroundImage: `url(${item.metaImageUrl})`,
-                      }
-                    : {}),
-                }}
-                title="NFT Image"
-              ></div>
+                  style={{
+                    ...(item.metaImageUrl
+                      ? {
+                          backgroundImage: `url(${item.metaImageUrl})`,
+                        }
+                      : {}),
+                  }}
+                  title="NFT Image"
+                >
+                  <span className="card" style={{ display: "block" }}></span>
+                </div>
+              </Link>
 
               <div className="w-full border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
                 <div className="mb-8">
